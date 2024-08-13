91f113c0080e7d69c1e27c0960c2664502021928
aaron-soto <aaron.m.soto1@gmail.com>
2024-08-13 10:26:00 -0700

    changed image paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,72 +20,72 @@ export const metadata = {
   description:
     "Aaron Soto is a frontend developer and designer who is always eager to learn. Keeping up with the latest tech trends is his forte.",
   icon: [
-    { url: "/assets/favicon/favicon.ico", type: "image/x-icon" },
-    { url: "/assets/favicon/favicon.svg", type: "image/svg+xml" },
+    { url: "/favicon/favicon.ico", type: "image/x-icon" },
+    { url: "/favicon/favicon.svg", type: "image/svg+xml" },
     {
-      url: "/assets/favicon/favicon-16x16.png",
+      url: "/favicon/favicon-16x16.png",
       type: "image/png",
       sizes: "16x16",
     },
     {
-      url: "/assets/favicon/favicon-32x32.png",
+      url: "/favicon/favicon-32x32.png",
       type: "image/png",
       sizes: "32x32",
     },
     {
-      url: "/assets/favicon/favicon-96x96.png",
+      url: "/favicon/favicon-96x96.png",
       type: "image/png",
       sizes: "96x96",
     },
     {
-      url: "/assets/favicon/favicon-192x192.png",
+      url: "/favicon/favicon-192x192.png",
       type: "image/png",
       sizes: "192x192",
     },
   ],
   apple: [
     {
-      url: "/assets/favicon/apple-touch-icon-57x57.png",
+      url: "/favicon/apple-touch-icon-57x57.png",
       sizes: "57x57",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-60x60.png",
+      url: "/favicon/apple-touch-icon-60x60.png",
       sizes: "60x60",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-72x72.png",
+      url: "/favicon/apple-touch-icon-72x72.png",
       sizes: "72x72",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-76x76.png",
+      url: "/favicon/apple-touch-icon-76x76.png",
       sizes: "76x76",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-114x114.png",
+      url: "/favicon/apple-touch-icon-114x114.png",
       sizes: "114x114",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-120x120.png",
+      url: "/favicon/apple-touch-icon-120x120.png",
       sizes: "120x120",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-144x144.png",
+      url: "/favicon/apple-touch-icon-144x144.png",
       sizes: "144x144",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-152x152.png",
+      url: "/favicon/apple-touch-icon-152x152.png",
       sizes: "152x152",
       type: "image/png",
     },
     {
-      url: "/assets/favicon/apple-touch-icon-180x180.png",
+      url: "/favicon/apple-touch-icon-180x180.png",
       sizes: "180x180",
       type: "image/png",
     },
