9a8db3022317baa5e32613fea6c2a91c678827c1
aaron-soto <aaron.m.soto1@gmail.com>
2024-08-21 13:05:05 -0700

    cleaning up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,19 +8,9 @@ const Footer = () => {
       <ul className=" text-white mb-4 mt-2">
         <ul className="flex gap-4">
           {/* <Link href="/snippets">snippets</Link> */}
-          <Link href="/blog">Blog</Link>
+          {/* <Link href="/blog">Blog</Link> */}
         </ul>
       </ul>
-      <div className="p-4 w-[55px] h-[55px] flex opacity-25 justify-center relative items-center  rounded-full bg-white aspect-square">
-        <Image
-          src="/images/as-logo.png"
-          alt="Hero"
-          height={40}
-          width={40}
-          className="mx-auto absolute "
-        />
-      </div>
-
       <p className="text-center text-gray-400 text-md">
         &copy; {new Date().getFullYear()} <strong>Aaron Soto</strong>. All
         rights reserved.
