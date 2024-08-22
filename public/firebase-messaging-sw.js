f574a3c0eb46bfb4eea79699d44284d29bfd936c
aaron-soto <aaron.m.soto1@gmail.com>
2024-08-22 07:34:58 -0700

    minor fixes?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,6 @@
-// Disable ESLint warnings for global variables
-// eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
-// eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
-// Firebase configuration object
 const firebaseConfig = {
   apiKey: "AIzaSyATtG9OYEXUFyiu0IuXNKsF_VkOrO2ucsk",
   authDomain: "aaron-portfolio-96729.firebaseapp.com",
@@ -15,15 +11,10 @@ const firebaseConfig = {
   measurementId: "G-ZVTJFKQQ0L",
 };
 
-// Initialize Firebase in the service worker
-// eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
 
-// Get an instance of Firebase Messaging
-// eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
-// Handle background messages
 messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
